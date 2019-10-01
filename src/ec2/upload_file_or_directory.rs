998d9ca529f2a294762cd5c7d1dd825f65d56fbe
IbrahimFadel <ibrahim.m.fadel@gmail.com>
2019-09-30 21:46:21 -0400

    uploading directories done. Really redundant (refactor it later)

--- src/ec2/upload_file_or_directory.rs
+++ src/ec2/upload_file_or_directory.rs
@@ -13,7 +13,7 @@ pub fn upload_file_or_directory(platform: String) {
     if file {
         upload_file(&path, &platform);
     } else if !file {
-        upload_directory(&path);
+        upload_directory(&path, &platform);
     } else {
         println!("Error, invalid path. Not a file or directory!");
         process::exit(1);
@@ -26,7 +26,7 @@ fn upload_file(path: &String, platform: &String) {
         let mut sure = String::new();
         input("Are you sure you want to upload to SCHOOL? (y/n)", &mut sure);
         if sure.trim() == "y" {
-            output = Command::new("rsync").args(&["-avL", "--progress", "-e", "ssh -i ~/devdir/ec2/ibrahimfadel.pem", "../rorkejs/src/rorke.js", "ec2-user@ec2-52-23-186-153.compute-1.amazonaws.com:~/data/school"]).output().expect("BAD");  
+            output = Command::new("rsync").args(&["-avL", "--progress", "-e", "ssh -i ~/devdir/ec2/ibrahimfadel.pem", path, "ec2-user@ec2-52-23-186-153.compute-1.amazonaws.com:~/data/school"]).output().expect("BAD");  
         } else {
             println!("Okay, exiting...");
             process::exit(1);
@@ -35,7 +35,7 @@ fn upload_file(path: &String, platform: &String) {
         let mut sure = String::new();
         input("Are you sure you want to upload to PC? (y/n)", &mut sure);
         if sure.trim() == "y" {
-            output = Command::new("rsync").args(&["-avL", "--progress", "-e", "ssh -i ~/devdir/ec2/ibrahimfadel.pem", "../rorkejs/src/rorke.js", "ec2-user@ec2-52-23-186-153.compute-1.amazonaws.com:~/data/pc"]).output().expect("BAD");
+            output = Command::new("rsync").args(&["-avL", "--progress", "-e", "ssh -i ~/devdir/ec2/ibrahimfadel.pem", path, "ec2-user@ec2-52-23-186-153.compute-1.amazonaws.com:~/data/pc"]).output().expect("BAD");
         } else {
             println!("Okay, exiting...");
             process::exit(1);
@@ -50,8 +50,33 @@ fn upload_file(path: &String, platform: &String) {
 
 }
 
-fn upload_directory(path: &String) {
+fn upload_directory(path: &String, platform: &String) {
+    let output;
+    if platform.trim().to_lowercase() == "mac" {
+        let mut sure = String::new();
+        input("Are you sure you want to upload to SCHOOL? (y/n)", &mut sure);
+        if sure.trim() == "y" {
+            output = Command::new("rsync").args(&["-avL", "--progress", "--exclude", "node_modules", "-e", "ssh -i ~/devdir/ec2/ibrahimfadel.pem", path, "ec2-user@ec2-52-23-186-153.compute-1.amazonaws.com:~/data/school"]).output().expect("BAD");  
+        } else {
+            println!("Okay, exiting...");
+            process::exit(1);
+        }
+    } else {
+        let mut sure = String::new();
+        input("Are you sure you want to upload to PC? (y/n)", &mut sure);
+        if sure.trim() == "y" {
+            output = Command::new("rsync").args(&["-avL", "--progress", "--exclude", "node_modules", "-e", "ssh -i ~/devdir/ec2/ibrahimfadel.pem", path, "ec2-user@ec2-52-23-186-153.compute-1.amazonaws.com:~/data/pc"]).output().expect("BAD");  
+        } else {
+            println!("Okay, exiting...");
+            process::exit(1);
+        }
+    }
+    if !output.status.success() {
+        let s = String::from_utf8_lossy(&output.stderr);
+        print!("rustc failed and stderr was:\n{}", s);
+    }
 
+    println!("{}", String::from_utf8_lossy(&output.stdout));
 }
 
 fn is_file(path: &String) -> bool {
