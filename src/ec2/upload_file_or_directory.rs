c3db6e98e2a13a5ffb01c4ca18775cdcfbf41eff
IbrahimFadel <ibrahim.m.fadel@gmail.com>
2019-10-01 23:48:21 -0400

    Refactored upload_file_or_directory.rs

--- src/ec2/upload_file_or_directory.rs
+++ src/ec2/upload_file_or_directory.rs
@@ -1,82 +1,38 @@
+use crate::inputs::input::input;
+
 use std::path::Path;
 use std::process;
-use std::process::Command;
-
-use crate::inputs::input::input;
 
 pub fn upload_file_or_directory(platform: String) {
     let mut path = String::new();
-    input("What is the path to your file/directory?", &mut path);
+    input("What is the path to your file/directiry?", &mut path);
     path = path.trim().to_string();
-    let file: bool = is_file(&path);
+    
+    let file = is_file(&path);
+    let loc = if platform.trim() == "mac" { String::from("~/data/school") } else { String::from("~/data/pc") };
 
-    if file {
-        upload_file(&path, &platform);
-    } else if !file {
-        upload_directory(&path, &platform);
-    } else {
-        println!("Error, invalid path. Not a file or directory!");
+    let mut sure = String::new();
+    input(&format!("{}{}", "Are you sure you want to upload to ", if platform.trim() == "mac" { "SCHOOL (y/n)" } else { "PC (y/n)" }), &mut sure);
+
+    if sure.trim() != "y" {
+        println!("Okay, Exiting");
         process::exit(1);
     }
-}
 
-fn upload_file(path: &String, platform: &String) {
     let output;
-    if platform.trim().to_lowercase() == "mac" {
-        let mut sure = String::new();
-        input("Are you sure you want to upload to SCHOOL? (y/n)", &mut sure);
-        if sure.trim() == "y" {
-            output = Command::new("rsync").args(&["-avL", "--progress", "-e", "ssh -i ~/devdir/ec2/ibrahimfadel.pem", path, "ec2-user@ec2-52-23-186-153.compute-1.amazonaws.com:~/data/school"]).output().expect("BAD");  
-        } else {
-            println!("Okay, exiting...");
-            process::exit(1);
-        }
+    if file {
+        output = process::Command::new("rsync").args(&["-avL", "--progress", "-e", "ssh -i ~/devdir/ec2/ibrahimfadel.pem", &path, &format!("{}{}", "ec2-user@ec2-52-23-186-153.compute-1.amazonaws.com:", loc)]).output().expect("Failed to upload");
     } else {
-        let mut sure = String::new();
-        input("Are you sure you want to upload to PC? (y/n)", &mut sure);
-        if sure.trim() == "y" {
-            output = Command::new("rsync").args(&["-avL", "--progress", "-e", "ssh -i ~/devdir/ec2/ibrahimfadel.pem", path, "ec2-user@ec2-52-23-186-153.compute-1.amazonaws.com:~/data/pc"]).output().expect("BAD");
-        } else {
-            println!("Okay, exiting...");
-            process::exit(1);
-        }
-    }
-    if !output.status.success() {
-        let s = String::from_utf8_lossy(&output.stderr);
-        print!("rustc failed and stderr was:\n{}", s);
+        output = process::Command::new("rsync").args(&["-avL", "--progress", "--exclude", "node_modules", "-e", "ssh -i ~/devdir/ec2/ibrahimfadel.pem", &path, &format!("{}{}", "ec2-user@ec2-52-23-186-153.compute-1.amazonaws.com:", loc)]).output().expect("Failed to upload");
     }
 
-  println!("{}", String::from_utf8_lossy(&output.stdout));
-
-}
-
-fn upload_directory(path: &String, platform: &String) {
-    let output;
-    if platform.trim().to_lowercase() == "mac" {
-        let mut sure = String::new();
-        input("Are you sure you want to upload to SCHOOL? (y/n)", &mut sure);
-        if sure.trim() == "y" {
-            output = Command::new("rsync").args(&["-avL", "--progress", "--exclude", "node_modules", "-e", "ssh -i ~/devdir/ec2/ibrahimfadel.pem", path, "ec2-user@ec2-52-23-186-153.compute-1.amazonaws.com:~/data/school"]).output().expect("BAD");  
-        } else {
-            println!("Okay, exiting...");
-            process::exit(1);
-        }
-    } else {
-        let mut sure = String::new();
-        input("Are you sure you want to upload to PC? (y/n)", &mut sure);
-        if sure.trim() == "y" {
-            output = Command::new("rsync").args(&["-avL", "--progress", "--exclude", "node_modules", "-e", "ssh -i ~/devdir/ec2/ibrahimfadel.pem", path, "ec2-user@ec2-52-23-186-153.compute-1.amazonaws.com:~/data/pc"]).output().expect("BAD");  
-        } else {
-            println!("Okay, exiting...");
-            process::exit(1);
-        }
-    }
     if !output.status.success() {
         let s = String::from_utf8_lossy(&output.stderr);
         print!("rustc failed and stderr was:\n{}", s);
     }
 
     println!("{}", String::from_utf8_lossy(&output.stdout));
+    
 }
 
 fn is_file(path: &String) -> bool {
